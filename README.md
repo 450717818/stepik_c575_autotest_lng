d536cb3050538a373b8629dd117fdc68c8c7d455
450717818 <sidevproject@gmail.com>
2022-04-14 10:52:15 +0300

    add files (code and description)

---
+++ README.md
@@ -0,0 +1,7 @@
+# stepik.org test_items
+
+Задание по курсу stepik.org «Автоматизация тестирования с помощью Selenium и Python"»
+(модуль 3, раздел 6, степ 9)
+
+Ссылка на урок с заданием: https://stepik.org/lesson/237240/step/9
+
---
+++ test_items.py
@@ -0,0 +1,15 @@
+import time # для проверки по п.2
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_exist_add_to_cart_button(browser):
+    # переход на нужную страницу
+    browser.get(link)
+    # time.sleep(30) # для проверки по п.2
+    # поиск кнопки добавления товара в корзину
+    WebDriverWait(browser, 3).until(EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "No add to cart button on page")
+
